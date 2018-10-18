63f798097aceadbb5816ae7d38192c0b65cab874
dan make <danmakes2022@gmail.com>
2018-10-18 23:24:26 +0330

    Button and TextInput ios

--- src/Button.js
+++ src/Button.js
@@ -1,50 +1,48 @@
 import React from 'react';
 import {
-    StyleSheet,
-    Text, TouchableNativeFeedback, View,TouchableOpacity,
-    Platform
+  StyleSheet,
+  Text, TouchableNativeFeedback, View,TouchableOpacity,
+  Platform
 } from 'react-native';
 import {colors,fontFamily} from "../config";
 
 export default class Button extends React.PureComponent {
-    render() {
-        if(Platform.OS === 'android')
-            return (
-                <TouchableNativeFeedback
-                    {...this.props}
-                    background={TouchableNativeFeedback.SelectableBackground()}>
-                    <View style={[styles.button, this.props.style]}>
-                        {this.props.children}
-                    </View>
-                </TouchableNativeFeedback>
-            );
+  render() {
+    if(Platform.OS === 'android')
+      return (
+        <TouchableNativeFeedback
+          {...this.props}
+          background={TouchableNativeFeedback.SelectableBackground()}>
+          <View style={[styles.button, this.props.style]}>
+            {this.props.children}
+          </View>
+        </TouchableNativeFeedback>
+      );
 
-        return (
-            <TouchableOpacity
-                {...this.props}
-                style={[styles.button, this.props.style]}>
-                <Text style={this.props.labelStyle}>
-                    {this.props.children}
-                </Text>
-            </TouchableOpacity>
-        );
-    }
+    return (
+      <TouchableOpacity
+        {...this.props}
+        style={[styles.button, this.props.style]}>
+        {this.props.children}
+      </TouchableOpacity>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
-    button:{
-        width: '100%',
-        height: 50,
-        borderRadius: 10,
-        borderWidth: 1,
-        borderColor: '#fff',
-        backgroundColor:'#fff',
-        justifyContent: 'center',
-        alignItems: 'center'
-    },
-    buttonText:{
-        color: colors.main,
-        fontSize: 20,
-        fontFamily
-    }
+  button:{
+    width: '100%',
+    height: 50,
+    borderRadius: 10,
+    borderWidth: 1,
+    borderColor: '#fff',
+    backgroundColor:'#fff',
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  buttonText:{
+    color: colors.main,
+    fontSize: 20,
+    fontFamily
+  }
 });
--- src/TextInput.js
+++ src/TextInput.js
@@ -1,27 +1,28 @@
 import React from 'react';
 import {
-    TextInput,
-    Platform
+  TextInput,
+  Platform
 } from 'react-native';
 import {fontFamily} from "../config";
 
 export default class Pext extends React.PureComponent {
-    render() {
-        return (
+  render() {
+    return (
 
-            <TextInput
-                placeholderTextColor='#ddd'
-                underlineColorAndroid='#ddd'
-                {...this.props}
-                style={[
-                    {
-                        fontFamily,
-                        color: '#fff',
-                        borderWidth: Platform.OS === 'ios' ? 1 : 0,
-                        borderColor: '#fff',
-                        fontSize: Platform.OS === 'android' ? 20 : 16,
-                        height: 60
-                    }, this.props.style]} />
-        );
-    }
+      <TextInput
+        placeholderTextColor='#ddd'
+        underlineColorAndroid='#ddd'
+        {...this.props}
+        style={[
+          {
+            fontFamily,
+            color: '#fff',
+            borderWidth: Platform.OS === 'ios' ? 1 : 0,
+            borderColor: '#fff',
+            fontSize: Platform.OS === 'android' ? 20 : 16,
+            height: 60,
+            textAlign: 'right'
+          }, this.props.style]} />
+    );
+  }
 }
