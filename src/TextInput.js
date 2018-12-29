6ccd74c8129b85239621376dee8894981b1aac68
dan make <danmakes2022@gmail.com>
2018-12-30 01:44:48 +0330

    persian numbers to english in textInput

--- src/TextInput.js
+++ src/TextInput.js
@@ -3,16 +3,25 @@ import {
   TextInput,
   Platform
 } from 'react-native';
-import {fontFamily} from "../config";
+import { fontFamily } from "../config";
 
 export default class Pext extends React.PureComponent {
   render() {
     return (
-
       <TextInput
         placeholderTextColor='#ddd'
         underlineColorAndroid='#ddd'
         {...this.props}
+        onChangeText={(str) => {
+          if (this.props.onChangeText) {
+            let persianNumbers = [ /۰/g, /۱/g, /۲/g, /۳/g, /۴/g, /۵/g, /۶/g, /۷/g, /۸/g, /۹/g ],
+              arabicNumbers = [ /٠/g, /١/g, /٢/g, /٣/g, /٤/g, /٥/g, /٦/g, /٧/g, /٨/g, /٩/g ];
+            for (let i = 0; i < 10; i++) {
+              str = str.replace(persianNumbers[ i ], i).replace(arabicNumbers[ i ], i);
+            }
+            this.props.onChangeText(str);
+          }
+        }}
         style={[
           {
             fontFamily,
@@ -22,7 +31,7 @@ export default class Pext extends React.PureComponent {
             fontSize: Platform.OS === 'android' ? 20 : 16,
             height: 60,
             textAlign: 'right'
-          }, this.props.style]} />
+          }, this.props.style ]}/>
     );
   }
 }
